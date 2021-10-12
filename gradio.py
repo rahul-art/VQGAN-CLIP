2fda2b893fcbd4924f21c46fb88746109e1044ba
RAHUL Danu <56379013+rahul-art@users.noreply.github.com>
2021-10-12 15:58:21 +0530

    Update gradio.py

--- gradio.py
+++ gradio.py
@@ -2,8 +2,6 @@ import subprocess
 subprocess.call("download_models.sh")
 subprocess.call(['git', 'clone', "https://github.com/openai/CLIP"])
 subprocess.call(['git', 'clone', "https://github.com/CompVis/taming-transformers"])
-git clone 'https://github.com/openai/CLIP'
-git clone 'https://github.com/CompVis/taming-transformers'
 import gradio as gr 
 def outbreak_forecast(Practice):
   python generate.py -p f"{Practice}"
